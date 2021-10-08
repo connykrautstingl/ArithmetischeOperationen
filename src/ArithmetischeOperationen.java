2673c2707928f88102efa3e6ce1a0ddba7b0f6a1
Connykrautstingl <s44784@edu.campus02.at>
2021-10-08 20:23:06 +0200

    first try of ArithmetischeOperationen

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,29 @@
+public class ArithmetischeOperationen {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 5;
+
+        System.out.println("a" + " + " + "b" + " = " + a + b);
+        System.out.println("a" + " - " + "b" + " = " + (a - b));
+        System.out.println("a" + " * " + "b" + " = " + (a * b));
+        System.out.println("a" + " / " + "b" + " = " + (a / b));
+        System.out.println("a" + " % " + "b" + " = " + (a % b));
+
+int add = a+b;
+int sub = a-b;
+int multi = a*b;
+int div = a/b;
+int mod = a%b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+        System.out.println(mod);
+
+
+    }
+
+}
